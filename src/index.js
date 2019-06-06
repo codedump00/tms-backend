f5ffc685e1e26d1a301f9bb0ab10a0021b3f50b7
codedump00 <blazinasis@gmail.com>
2019-06-06 22:58:18 +0545

    structure changes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,15 @@ app.use(bodyParser.json())
 
 app.use(express.static("public"))
 /*------------------*/
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With,Content-Type, Accept, Authorization')
+  if(req.method === 'OPTIONS'){
+      res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET')
+      return res.status(200).json({})
+  }
+})
 
 /*Route Declaration*/
 
@@ -53,13 +62,3 @@ app.use((error, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`)
 })
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With,Content-Type, Accept, Authorization')
-//     if(req.method === 'OPTIONS'){
-//         res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'PATCH', 'DELETE', 'GET')
-//         return res.status(200).json({})
-//     }
-// })
